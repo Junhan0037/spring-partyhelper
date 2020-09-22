ed2e30a515a5e99131c2ec607427e599bc79537f
Junhan0037 <wnsgks0037@naver.com>
2020-09-22 15:53:29 +0900

    Update 첫 화면 수정

--- src/main/java/com/partyhelper/infra/config/SecurityConfig.java
+++ src/main/java/com/partyhelper/infra/config/SecurityConfig.java
@@ -32,26 +32,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .csrf().disable()
 //                .headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션을 disable
 //                .and()
-                    .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "/login", "/login-user", "/login-provider",
-                            "/sign-up", "/sign-up-user", "/sign-up-provider", "/check-email", "/check-email-token", "/search/event", "/nginx",
-                            "/email-login", "/check-email-login", "/login-link", "/login-by-email", "/favicon.ico", "/resources/**", "/error").permitAll() // 전체 열람 권한
-                    .antMatchers("/settings/account", "/settings/password").hasAnyRole("USER", "PROVIDER", "ADMIN")
-                    .antMatchers("/new-event").hasAnyRole("USER", "OAUTH", "ADMIN")
-                    .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // USER 권한을 가진 사람만 가능
-                    .antMatchers(HttpMethod.GET, "/profile/*").permitAll()
-                    .anyRequest().authenticated() // anyRequest() : 설정한 값들 이외 나머지 URL. => 나머지 URL들은 모두 인증과정을 거친다
+                .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "/login", "/login-user", "/login-provider",
+                        "/sign-up", "/sign-up-user", "/sign-up-provider", "/check-email", "/check-email-token", "/search/event", "/nginx",
+                        "/email-login", "/check-email-login", "/login-link", "/login-by-email", "/favicon.ico", "/resources/**", "/error").permitAll() // 전체 열람 권한
+                .antMatchers("/settings/account", "/settings/password").hasAnyRole("USER", "PROVIDER", "ADMIN")
+                .antMatchers("/new-event").hasAnyRole("USER", "OAUTH", "ADMIN")
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // USER 권한을 가진 사람만 가능
+                .antMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated() // anyRequest() : 설정한 값들 이외 나머지 URL. => 나머지 URL들은 모두 인증과정을 거친다
                 .and()
-                    .formLogin()
-                        .loginPage("/login-user").permitAll()
+                .formLogin()
+                .loginPage("/login-user").permitAll()
                 .and()
-                    .logout()
-                        .logoutSuccessUrl("/") // 로그아웃 성공 시 "/" 주소로 이동
+                .logout()
+                .logoutSuccessUrl("/") // 로그아웃 성공 시 "/" 주소로 이동
                 .and()
-                    .oauth2Login() // OAuth2 로그인 기능에 대한 여러 설정의 진입점
-                        .loginPage("/oauth")
-                        .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들 담당
-                            .userService(customOAuth2UserService); // userService() : 소셜 로그인 성공 시 후속 조치를 진행할 구현체 등록
+                .oauth2Login() // OAuth2 로그인 기능에 대한 여러 설정의 진입점
+                .loginPage("/oauth")
+                .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정들 담당
+                .userService(customOAuth2UserService); // userService() : 소셜 로그인 성공 시 후속 조치를 진행할 구현체 등록
 
         http.rememberMe()
                 .userDetailsService(accountService)
@@ -62,6 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
                 .mvcMatchers("/node_modules/**")
+                .mvcMatchers("/assets/**")
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()); // static resource 인증 X (이미지 등)
     }
 
